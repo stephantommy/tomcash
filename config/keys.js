3d288e005282e8d0005b17265ed481eecba770bf
STEPHANUS TOMMY TOMBOKAN <U067117@bca>
2020-09-22 10:17:28 +0700

    channge port to 8080

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
     host: process.env.HOST || '0.0.0.0',
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 8080,
 
     login: 'http://' + (process.env.LOGIN_SERVICE_HOST || 'localhost') + ':' + (process.env.LOGIN_SERVICE_PORT || '3002') + '/api/login',
     cashpointList: 'http://' + (process.env.CASHPOINT_SERVICE_HOST || 'localhost') + ':' + (process.env.CASHPOINT_SERVICE_PORT || '3001') + '/api/cashpoint',
